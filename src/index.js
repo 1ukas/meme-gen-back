ba1088a3b97676a1e41875ae8cc014c4189586cc
Lukas <lukasmilten@gmail.com>
2019-09-14 20:56:29 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'using strict'
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const expressFormidable = require('express-formidable');
+
+const app = express();
+
+const imageGen = require('./routes/imageGen');
+
+app.use(cors());
+app.use(expressFormidable());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(imageGen);
+
+// Handle 404:
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.info(`Server is running on port: ${PORT}`));
